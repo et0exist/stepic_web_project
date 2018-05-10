912883caf2784106d2566f4773631fec6a68cdc6
et0exist <et0exist@gmail.com>
2018-05-11 02:09:42 +0300

    no hope

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
-sudo rm -r /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-#sudo rm /etc/nginx/sites-enabled/default 2> /dev/null
+sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
