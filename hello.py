52dc7f8778c18fdba702e47ae7d668d9eaa03637
MaxVeter <et0exist@gmail.com>
2018-05-17 20:30:04 +0300

    no_hope

--- hello.py
+++ hello.py
@@ -1,15 +1,10 @@
 def wsgi_application(environ, start_response):
-    from cgi import parse_qs
-
-    params = parse_qs(environ['QUERY_STRING'])
-    body = ''
-    for i in params:
-        body += params[i][0] + '\n'
-    body = bytes(body, encoding = 'utf-8')
-
+    
+    body = [bytes(i, encoding = 'utf-8') for i in environ['QUERY_STRING'].split(sep='&')]
+    
     status = '200 OK'
     headers = [
             ('Content-Type', 'text/plain')
     ]
     start_response(status, headers)
-    return [body]
+    return iter(body)
