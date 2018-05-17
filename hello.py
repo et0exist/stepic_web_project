57fd6dff34f4cf8146ab3c9e29ed7da84b8ccc5f
MaxVeter <et0exist@gmail.com>
2018-05-17 20:39:41 +0300

    no_no_hope

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def wsgi_application(environ, start_response):
     
-    body = [bytes(i, encoding = 'utf-8') for i in environ['QUERY_STRING'].split(sep='&')]
+    body = [bytes(i + '\n', encoding = 'utf-8') for i in environ['QUERY_STRING'].split(sep='&')]
     
     status = '200 OK'
     headers = [
