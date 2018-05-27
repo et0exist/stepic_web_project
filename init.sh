1367f368a82bd2798e5b8c2dfbf3715604cc642e
et0exist <et0exist@gmail.com>
2018-05-27 03:10:49 +0300

    check

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 #sudo gunicorn -w 4 hello:wsgi_application -c /etc/gunicorn.d/test
-#sudo /etc/init.d/mysql start
+sudo /etc/init.d/mysql start
