aee8f9aa139d5ea546a6f067928a6619edbac380
et0exist <et0exist@gmail.com>
2018-05-31 03:07:16 +0300

    finished_django

---
+++ ask/ask/urls.py
@@ -0,0 +1,29 @@
+"""ask URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, re_path
+from qa import views
+
+urlpatterns = [
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', views.home),
+    re_path(r'^login/$', views.login),
+    re_path(r'^signup/$', views.signup),
+    re_path(r'^ask/$', views.ask),
+    re_path(r'^popular/$', views.popular),
+    re_path(r'^new/$', views.new),
+    re_path(r'^question/', views.question),
+]
---
+++ ask/qa/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+
+# Create your views here.
+# from django.http import HttpResponse
+# def test(request, *args, **kwargs):
+#     return HttpResponse('OK')
+
+from django.http import HttpResponse
+
+
+def home(request, *args, **kwargs):
+    return HttpResponse('Home page')
+
+
+def login(request, *args, **kwargs):
+    return HttpResponse('Login page')
+
+
+def signup(request, *args, **kwargs):
+    return HttpResponse('Signup page')
+
+
+def ask(request, *args, **kwargs):
+    return HttpResponse('Ask page')
+
+
+def popular(request, *args, **kwargs):
+    return HttpResponse('Popular page')
+
+
+def new(request, *args, **kwargs):
+    return HttpResponse('New page')
+
+def question(request, *args, **kwargs):
+    return HttpResponse('Question {}'.format(' '.join(request.path.split('/')[2:])))
