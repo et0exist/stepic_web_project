55936c234526c784852262eb0fed5d74b9176dff
et0exist <et0exist@gmail.com>
2018-09-22 00:56:10 +0300

    old_django

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,29 +1,29 @@
 """ask URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, re_path
 from qa import views
 
 urlpatterns = [
-    re_path(r'^admin/', admin.site.urls),
-    re_path(r'^$', views.home),
-    re_path(r'^login/', views.login),
-    re_path(r'^signup/', views.signup),
-    re_path(r'^question/', views.question),
-    re_path(r'^ask/', views.ask),
-    re_path(r'^popular/', views.popular),
-    re_path(r'^new/', views.new),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home),
+    url(r'^login/', views.login),
+    url(r'^signup/', views.signup),
+    url(r'^question/', views.question),
+    url(r'^ask/', views.ask),
+    url(r'^popular/', views.popular),
+    url(r'^new/', views.new),
 ]
--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+#sudo ln -sf /usr/bin/python3 /usr/bin/python
+#sudo pip3 install --upgrade gunicorn
+#sudo apt install nginx
+
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo ln -sf /home/box/web/etc/gunicorn_wsgi.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #gunicorn -c /home/box/web/etc/gunicorn_wsgi.conf hello:wsgi_application
 gunicorn -c /home/box/web/etc/gunicorn_django.conf ask.wsgi:application
