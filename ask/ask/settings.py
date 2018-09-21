61403e8f1a269fdbb2b28f8248d5ff06b0fb121f
et0exist <et0exist@gmail.com>
2018-09-21 19:30:30 +0300

    refreshed

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w_%!n-^@j_7m%-had+4$do643^&n%mf@6i7+j-j_q!0z+ij1nt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'qa',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ask',
+        'USER': 'stepic',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from qa import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', views.home),
+    re_path(r'^login/', views.login),
+    re_path(r'^signup/', views.signup),
+    re_path(r'^question/', views.question),
+    re_path(r'^ask/', views.ask),
+    re_path(r'^popular/', views.popular),
+    re_path(r'^new/', views.new),
 ]
