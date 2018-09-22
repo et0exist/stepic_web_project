360e1222f9d31193314fb4a728e498e6a7c4f62b
et0exist <et0exist@gmail.com>
2018-09-22 16:00:39 +0300

    added_some_models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=140)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+    likes = models.IntegerField()
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
